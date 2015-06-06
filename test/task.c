c27fd17dd72fcddf72bb4a0fe615afcfc8a434e2
vizee <vizee@live.cn>
2015-06-07 01:32:08 +0800

    简单实现

---
+++ test/task.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "../thrpool.h"
+
+static void task_proc(void *param)
+{
+	printf("thread %p param %p\n", (void *)pthread_self(), param);
+}
+
+int main()
+{
+	thrpool_t pool;
+	int i;
+	
+	pool = thrpool_create(2);
+	for (i = 0; i < 40; i++) {
+		thrpool_add_task(pool, task_proc, (void *)i);
+	}
+	printf("begin\n");
+	thrpool_destroy(pool, 1);
+	printf("end\n");
+	return 0;
+}
+
